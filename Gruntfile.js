982415ad4f8cee04c34de84e1179c79bd2c5e539
Tadaking <tadaking@pixiv.com>
2014-08-28 15:57:30 +0900

    grunt-slim

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,13 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    slim: {
+      dist: {
+        files: {
+          'dist/style_guide.html': 'src/slim/style_guide.slim'
+        }
+      }
+    },
+
     stylus: {
       app: {
         src : ['src/stylus/*.styl'],
@@ -9,6 +17,7 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-slim');
 
-  grunt.registerTask('default', 'stylus');
+  grunt.registerTask('default', 'slim');
 };
